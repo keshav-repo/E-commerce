5b49de5b5d4658379edadb958ed36f96505db8f3
Keshav Kumar <keshav.k987@gmail.com>
2024-07-02 13:59:39 +0530

    adding favicon and meta information

--- frontend/app/product/page.tsx
+++ frontend/app/product/page.tsx
@@ -1,6 +1,13 @@
+import Head from 'next/head';
 import ProductDetails from '@/app/ui/product-details';
 import { fetchProduct } from '@/app/lib/fetchProduct';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Product page information',
+  description: 'some product information',
+};
 
 export default async function Page({
   params,
