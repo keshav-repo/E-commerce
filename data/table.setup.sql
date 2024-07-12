5f19d98bcd85a7263d9795ba997fe42bea016e92
Keshav Kumar <keshav.k987@gmail.com>
2024-07-13 01:06:19 +0530

    table setup

--- data/table.setup.sql
+++ data/table.setup.sql
@@ -1,11 +1,19 @@
--- create database
 CREATE DATABASE ecommerce;
 
 -- connect to db
 -- \ c ecommerce
--- create table for product
+
+CREATE TABLE users (
+    userId SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    name VARCHAR(100),
+    profilePhoto TEXT 
+);
+
 CREATE TABLE product (
-    product_id SERIAL PRIMARY KEY,
+    productId SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price NUMERIC(10, 2) NOT NULL,
@@ -13,21 +21,33 @@ CREATE TABLE product (
     company VARCHAR(100) NOT NULL,
     images JSONB,
     additional_info JSONB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	gender varchar(30)
 );
 
-alter table
-    product
-add
-    column gender varchar(30);
-
-CREATE TABLE users (
-    userId SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    password VARCHAR(100),
-    -- Make this nullable to accommodate OAuth users
-    email VARCHAR(100) UNIQUE,
-    name VARCHAR(100),
-    profilePhoto TEXT -- Use TEXT for potentially long URLs or base64 encoded images
+CREATE TABLE carts (
+    cartId SERIAL PRIMARY KEY,
+    userId INT NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userId) REFERENCES users(userId)
 );
+
+CREATE TABLE cartItems (
+    cartItemId SERIAL PRIMARY KEY,
+    cartId INT NOT NULL,
+    productId INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (cartId) REFERENCES carts(cartId) ON DELETE CASCADE,
+    FOREIGN KEY (productId) REFERENCES product(productId)
+);
+
+CREATE INDEX idx_cart_user ON carts(cartId);
+CREATE INDEX idx_cart_items_cart ON cartItems(cartId);
+CREATE INDEX idx_cart_items_product ON cartItems(productId);
+
+
